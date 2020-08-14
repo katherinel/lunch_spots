291946934f2a04af5bcfc639fcab03b9455371c7
Katherine Leipper <katherine.leipper@bugcrowd.com>
2020-08-13 20:41:57 -0700

    Create new rails api with JWT authentication

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,41 @@
+class ApplicationController < ActionController::API
+  before_action :authorized
+
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  end
+
+  def encode_token(payload)
+    JWT.encode(payload, 'supersecret')
+  end
+
+  private
+
+  def auth_header
+    # { Authorization: 'Bearer <token>' }
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+    return unless auth_header
+
+    token = auth_header.split(' ')[1]
+    # header: { 'Authorization': 'Bearer <token>' }
+    begin
+      JWT.decode(token, 'supersecret', true, algorithm: 'HS256')
+    rescue JWT::DecodeError
+      nil
+    end
+  end
+
+  def logged_in_user
+    return unless decoded_token
+
+    user_id = decoded_token[0]['user_id']
+    @user = User.find_by(id: user_id)
+  end
+
+  def logged_in?
+    !!logged_in_user
+  end
+end
